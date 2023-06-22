e5fc89e2ae88bbc7f93bc276f1a0765d23b7b126
Olafffffff <136884616+Olafffffff@users.noreply.github.com>
2023-06-22 21:15:54 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,38 +1,15 @@
-<?php
-session_start();
-
-if (isset($_POST['difficulty'])) {
-    $_SESSION['difficulty'] = $_POST['difficulty'];
-    header('Location: game.php');
-    exit;
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        body {
-        #grad1 {
-            width: 1890px;
-            height: 1000px;
-            background-image: linear-gradient(white, lightblue);
-        }
-    </style>
-    <title>Gra Wisielec</title>
-</head>
-<body>
-<p><center>
-<h1><p style="font-family:Times New Roman"> Gra Wisielec </p></h1>
-<form method="post" action="">
-    <label for="difficulty">Wybierz poziom trudności:</label>
-   <select id="difficulty" name="difficulty">
-        <option value="easy">Łatwy</option>
-        <option value="hard">Trudny</option>
-    </select>
-     <input type="submit" value="Rozpocznij grę">
+<form class="space-y-4" method="post" action="index.php">
+	    <input type="hidden" name="start" value="1">
+            <div class="flex items-center">
+                <label for="difficulty" class="mr-2">Wybierz poziom trudności:</label>
+                <select id="difficulty" name="difficulty" class="border border-gray-300 rounded px-4 py-2">
+                    <option value="easy">Łatwy</option>
+                    <option value="hard">Trudny</option>
+                </select>
+            </div>
+            <div class="flex justify-center">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                    Start
+                </button>
+            </div>
 </form>
-    </center></p>
-<div id="grad1"></div>
-</body>
-</html>
-
